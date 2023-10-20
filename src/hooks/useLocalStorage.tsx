c6b77678248179be68475eee0a5c72af3e0f0f68
hewayman <hewayman@gmail.com>
2023-10-20 18:16:28 -0400

    fix useLocalStorage hook

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -4,20 +4,22 @@ import { useEffect, useState } from 'react';
 
 const useLocalStorage = (key: string, defaultValue: string) => {
   const [value, setValue] = useState(() => {
-    try {
-      const storedValue = localStorage.getItem(key);
+    if (typeof window !== 'undefined') {
+      try {
+        const storedValue = localStorage.getItem(key);
 
-      if (storedValue) {
-        return JSON.parse(storedValue);
-      } else {
+        if (storedValue) {
+          return JSON.parse(storedValue);
+        } else {
+          localStorage.setItem(key, JSON.stringify(defaultValue));
+
+          return defaultValue;
+        }
+      } catch (error) {
         localStorage.setItem(key, JSON.stringify(defaultValue));
 
         return defaultValue;
       }
-    } catch (error) {
-      localStorage.setItem(key, JSON.stringify(defaultValue));
-
-      return defaultValue;
     }
   });
 
